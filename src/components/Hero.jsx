0fbd5b6b9396088bc10b469abca8edefb42650b5
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-03-25 22:34:15 -0600

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 
 
-function hero() {
+const hero = () => {
   return (
-    <div>hero</div>
+    <section className="w-full nav-height bg-black">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
   )
 }
 
