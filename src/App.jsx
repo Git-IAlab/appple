d83caa833f11bf7dfc1e7889d4c056423f0a9faa
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-06-11 23:32:25 -0500

    Sentry Init

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
 import Model from "./components/Model";
+import * as Sentry from '@sentry/react'
 
 const App = ()=> {
 
@@ -15,4 +16,4 @@ const App = ()=> {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//...
+import * as Sentry from "@sentry/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
